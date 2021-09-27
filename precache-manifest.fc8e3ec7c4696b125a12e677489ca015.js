17db25c4bf0e5ad1bc10ee3f32a870dd56a3f368
Roman Minchenko <minchenko.poma@gmail.com>
2021-09-27 19:44:52 +0300

    Updates

---
+++ precache-manifest.fc8e3ec7c4696b125a12e677489ca015.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0f6e48c7c5fae55bfca7b00d122cee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca64fb9d5fecc4afa33b",
+    "url": "./static/js/main.94750ee4.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.fc8e3ec7c4696b125a12e677489ca015.js"
 );
 
 self.addEventListener('message', (event) => {
